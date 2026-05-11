ac780c0dbb8ad9a41a9503c215c9cc2bafac4232
Junayed Islam <junayedislamjadir5721@gmail.com>
2026-05-12 01:31:10 +0600

    Add initial Team Lead and Admin dashboard pages

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "task_project_management_tool";
+
+$conn = mysqli_connect($host, $username, $password, $database);
+
+if (!$conn) {
+    die("Database connection failed: " . mysqli_connect_error());
+}
+
+?>
